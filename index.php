d5c9bec643f76ce4c4b0ef595536d242c2b9c577
David Rees <d.w.rees.03@gmail.com>
2016-09-13 17:04:28 +0100

    add typography page

---
+++ index.php
@@ -0,0 +1,22 @@
+<? include('./includes/header.html');?>
+        <img src="img/12-col.png" class="hero" />
+        <div class="row">
+          <div class="medium-6 columns">
+            <img src="img/6-col.png" />
+          </div>
+          <div class="medium-6 columns">
+           <img src="img/6-col.png" />
+          </div>
+        </div>
+        <div class="row">
+          <div class="medium-4 columns">
+            <img src="img/4-col.png" />
+          </div>
+          <div class="medium-4 columns">
+            <img src="img/4-col.png" />
+          </div>
+          <div class="medium-4 columns">
+           <img src="img/4-col.png" />
+          </div>
+        </div>
+<? include('./includes/footer.html');?>
