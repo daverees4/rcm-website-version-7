ba6005467a58331b2c4aec085083f9508e50380c
David Rees <d.w.rees.03@gmail.com>
2016-09-14 15:40:12 +0100

    create typography tests

---
+++ typography-futura-plus.php
@@ -0,0 +1,35 @@
+<? include('./includes/header.html');?>
+        <div class="row">
+          <div class="text-test-area columns futura-plus">
+            <h1>Typography Settings</h1>
+            <hr />
+            <h2>Headings</h2>
+            <hr />
+            <p>This page uses Futura Plus</p>
+            <hr />
+            <h1>This is h1</h1>
+            <h2>This is h2</h2>
+            <h3>This is h3</h3>
+            <h4>This is h4</h4>
+            <h5>This is h5</h5>
+            <h6>This is h6</h6>
+            <hr />
+            <h2>Paragraph Text</h2>
+            <hr />
+            <p class="lead">The RCM Creative Careers Centre sources and manages a range of external opportunities that offer practical experience in working as a musician.</p>
+            <p>Inventive programming, immaculate presentation and a highly professional attitude are all essential requirements for musicians undertaking this work, and the team guides and supports you through this process offering feedback following each event.</p>
+
+<p>Our Creative Careers Centre Assistant is committed to ensuring that relevant jobs and opportunities in the music business are featured on our extensive noticeboards and in our popular weekly jobs e-bulletin, bringing opportunities direct to your inbox whether you are a current student or an RCM graduate.</p>
+
+<p>The Creative Careers Centre's team leads units and seminars on the BMus programme raising many of the key issues concerning professional musicians. Topics include promoting yourself, applying for work, organising effective concerts and planning education projects.</p>
+
+<p>An essential part of many musicians' careers, teaching is widely valued among its exponents as a creative and worthwhile way to share your musical passion and expertise with a new generation. The RCM Creative Careers Centre runs a popular teaching service which matches members of the public who want to learn with RCM students who are able to teach. Every week we receive requests for private music teachers of all types. If you have the skills, commitment and experience you can be considered for teaching opportunities as they arise.</p>
+
+
+
+          </div>
+          
+        </div>
+<? include('./includes/footer.html');?>
+
+
