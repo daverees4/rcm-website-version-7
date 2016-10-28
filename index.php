4f1749cd4f02c732845041edd17084291ac706e8
David Rees <d.w.rees.03@gmail.com>
2016-10-28 11:55:59 +0100

    Friday 28th October Progress

--- index.php
+++ index.php
@@ -1,22 +1,11 @@
 <? include('./includes/header.html');?>
-        <img src="img/12-col.png" class="hero" />
+        <img src="img/hero.jpg" class="hero" />
         <div class="row">
           <div class="medium-6 columns">
             <img src="img/6-col.png" />
           </div>
           <div class="medium-6 columns">
            <img src="img/6-col.png" />
-          </div>
-        </div>
-        <div class="row">
-          <div class="medium-4 columns">
-            <img src="img/4-col.png" />
-          </div>
-          <div class="medium-4 columns">
-            <img src="img/4-col.png" />
-          </div>
-          <div class="medium-4 columns">
-           <img src="img/4-col.png" />
           </div>
         </div>
 <? include('./includes/footer.html');?>
