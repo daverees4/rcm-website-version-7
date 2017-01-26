eede888c3e1fcb4e31db4f9150e792ce453097bb
David Rees <d.w.rees.03@gmail.com>
2017-01-26 15:39:04 +0000

    js update

--- js/min/app-min.js
+++ js/min/app-min.js
@@ -107,6 +107,24 @@ $(document).ready(function() {
     });
 });
 
+(function($) {
+    $.fn.shuffle = function() {
+        var allElems = this.get(), getRandom = function(max) {
+            return Math.floor(Math.random() * max);
+        }, shuffled = $.map(allElems, function() {
+            var random = getRandom(allElems.length), randEl = $(allElems[random]).clone(true)[0];
+            allElems.splice(random, 1);
+            return randEl;
+        });
+        this.each(function(i) {
+            $(this).replaceWith($(shuffled[i]));
+        });
+        return $(shuffled);
+    };
+})(jQuery);
+
+$("owl-item").shuffle();
+
 (function(E, q) {
     "function" === typeof define && define.amd ? define([], q) : "object" === typeof exports ? module.exports = q() : q();
 })(this, function() {
