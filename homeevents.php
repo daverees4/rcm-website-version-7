194db8edeb0882fa67b8c99342dbb3d8f7f61553
David Rees <d.w.rees.03@gmail.com>
2017-02-01 15:34:13 +0000

    what son this week spacing

---
+++ homeevents.php
@@ -0,0 +1,44 @@
+<? include('./includes/header.html');?>
+
+
+         <div class="row">
+		<div class="homeevents-topbar medium-12 columns">
+          <div class="owl-icons">
+            <i class="float-right icon-arrow-right next"></i>
+            <i class="float-right icon-arrow-left previous"></i>
+          </div>
+          <h2>What's on this week</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div id="owl-example" class="owl-carousel owl-theme" style="opacity: 1; display: block;">
+          <div class="owl-wrapper-outer"><div class="owl-wrapper" style="width: 153440px; left: 0px; display: block; transition: all 0ms ease; transform: translate3d(0px, 0px, 0px);"><div class="owl-item" style="width: 560px;"><div class="columns homeevents-panel">
+
+	<a href="/events/listings/details/?id=1210869"><img src="http://www.rcm.ac.uk/included/eventimages/chihoicheung.jpg"></a>
+	<p class="index-title"><a href="/events/listings/details/?id=1210869">RCM at Steinway Hall</a></p>
+    <p>01 February 2017, 1:00pm<br>
+    Steinway Hall, 44 Marylebone Lane, London W1U 2DB</p>
+</div></div><div class="owl-item" style="width: 560px;"><div class="columns">
+
+	<a href="/events/listings/details/?id=1121878"><img src="http://www.rcm.ac.uk/included/eventimages/sparkslunchtime2.jpg"></a>
+	<p class="index-title"><a href="/events/listings/details/?id=1121878">RCM Sparks Lunchtime Concert for Schools</a></p>
+    <p>01 February 2017, 1:05pm<br>
+    Amaryllis Fleming Concert Hall</p>
+</div></div><div class="owl-item" style="width: 560px;"><div class="columns">
+
+	<a href="/events/listings/details/?id=1178733"><img src="http://www.rcm.ac.uk/included/eventimages/bigbandnew2.jpg"></a>
+	<p class="index-title"><a hr
+  	ef="/events/listings/details/?id=1178733">RCM Friends Pre-Concert Supper</a></p>
+    <p>01 February 2017, 5:45pm<br>
+    Ognisko, 55 Prince's Gate, Exhibition Road, London SW7 2PG</p>
+</div></div><div class="owl-item" style="width: 560px;">
+</div>
+</div>
+    </div>
+        </div>
+    </div>
+        </div>
+<? include('./includes/footer.html');?>
+
+
+
