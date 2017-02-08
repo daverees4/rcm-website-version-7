7a349e20b2b68173d3897b4e08e49954bb863ac9
David Rees <d.w.rees.03@gmail.com>
2017-02-08 16:21:41 +0000

    css

---
+++ accom.php
@@ -0,0 +1,309 @@
+<? include('./includes/header.html');?>
+
+                      
+
+ <div class="grey-border-full"></div>
+      <div id="tertiary-mobile-container" class="hide-for-medium">        
+      <div class="row">
+          <div id="tertiary-menu-mobile" class="columns">          
+             <ul class="menu vertical tertiary-menu">
+               <li class="menu-highlight hide"><a href="/newsitebuildarea/life/">Life at the RCM<i class="float-right icon-cross"></i></a></li>
+                <li><a href="/newsitebuildarea/life/location/">Location</a><i class="float-right icon-arrow-right"></i></li> <li class="hide">Accommodation</li> <li class="hide"><a href="/newsitebuildarea/life/preparingforprofession/">Preparing for the profession</a></li> <li class="hide"><a href="/newsitebuildarea/life/studentsupport/">Student support</a></li>
+            </ul>
+          </div>
+        </div>
+        </div>
+        <div class="blue-border-full"></div>
+
+
+    <div class="row header-spacer">
+      <div class="large-12 columns">
+      <h1>Accommodation</h1>
+      </div>
+    </div>
+
+
+        <div class="row">
+          <div class="medium-4 medium-push-8 large-3 large-offset-1 large-push-8 columns show-for-medium">
+             <ul class="menu vertical tertiary-menu tertiary-right">
+               <li class="menu-highlight"><a href="/newsitebuildarea/life/">Life at the RCM</a></li>
+                <li><a href="/newsitebuildarea/life/location/">Location</a></li> <li>Accommodation</li> <li><a href="/newsitebuildarea/life/preparingforprofession/">Preparing for the profession</a></li> <li><a href="/newsitebuildarea/life/studentsupport/">Student support</a></li>
+            </ul>
+          </div>
+          <div class="medium-8 medium-pull-4 columns">  <img src="/media/_Students outside PCV 16x9.jpg" alt="Two RCM students leaving prince consort village" title="Two RCM students leaving prince consort village" class="detail-hero"><div class="main-page-content" id="">
+ <h5>The Royal College of Music’s hall of residence, Prince Consort Village, provides high-quality accommodation for up to 400 students.</h5>
+</div><div class="main-page-content" id="">
+ <p>The new building on Goldhawk Road&nbsp;opened in spring 2016 and comprises:</p>
+<ul>
+<li>177 studio&nbsp;apartments</li>
+<li>170 en-suite rooms</li>
+<li>63 standard rooms</li>
+</ul>
+<p class="paragraph-topup">Many first year student choose to live here so they can meet other students and grow accustomed to living in London.</p>
+</div><div class="row feature-box-row">
+                <div class="row">
+                  <div class="feature-box">
+                    <div class="small-12 columns feature-box-inner">
+                     
+                      <div class="feature-box-content">
+                             
+                      <p>If you would like to find out more or register your interest, please visit the&nbsp;Prince Consort Village&nbsp;website.</p>
+<p><a class="button" href="https://www.mystudentvillage.com/uk/prince-consort-village/" target="_blank">Prince Consort Village website</a></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+<div class="main-page-content" id="">
+ <p>Prince Consort Village is staffed 24 hours a day, and the&nbsp;in-house team and residence manager are dedicated to&nbsp;the pastoral care and wellbeing of residents.</p>
+<p>Regular events are organised and the Reception team is on hand daily to make students’ lives as easy and comfortable as possible.</p>
+</div><div class="main-page-content" id="">
+ <p>Students who would like to register their interest should <a href="https://www.mystudentvillage.com/uk/prince-consort-village/rcm-students/contact/" target="_blank">contact</a> Prince Consort Village directly and not the Royal College of Music.</p>
+</div><div class="row">
+	<div class="small-12 columns">
+		<div class="gallery  owl-carousel owl-theme" style="opacity: 1; display: block;">
+
+<div class="owl-wrapper-outer"><div class="owl-wrapper" style="width: 14840px; left: 0px; display: block; transition: all 0ms ease; transform: translate3d(0px, 0px, 0px);"><div class="owl-item" style="width: 742px;"><div class="gallery-item"><img src="/media/_PCV students playing pool 6x4.jpg"><p>Students relax by playing pool  1 of 10</p></div></div><div class="owl-item" style="width: 742px;"><div class="gallery-item"><img src="/media/_PCV Bedroom 6x4.jpg"><p>One of the Prince Consort Village bedrooms  2 of 10</p></div></div><div class="owl-item" style="width: 742px;"><div class="gallery-item"><img src="/media/_PCV Student In Coridoor 6x4.jpg"><p>The facilities are bright and airy  3 of 10</p></div></div><div class="owl-item" style="width: 742px;"><div class="gallery-item"><img src="/media/_PCV students in Kitchen 6x4.jpg"><p>Shared kitchens are a great place to socialise  4 of 10</p></div></div><div class="owl-item" style="width: 742px;"><div class="gallery-item"><img src="/media/_PCV violins practice room 6x4.jpg"><p>There are on-site practice facilities for RCM students  5 of 10</p></div></div><div class="owl-item" style="width: 742px;"><div class="gallery-item"><img src="/media/_PCV Laundry 6x4.jpg"><p>An on-site laundry room makes life easier  6 of 10</p></div></div><div class="owl-item" style="width: 742px;"><div class="gallery-item"><img src="/media/_PCV piano practice room 6x4.jpg"><p>The practice rooms are equipped with pianos  7 of 10</p></div></div><div class="owl-item" style="width: 742px;"><div class="gallery-item"><img src="/media/_Students at PCV socialising 6x4.jpg"><p>A variety of social activities is on offer  8 of 10</p></div></div><div class="owl-item" style="width: 742px;"><div class="gallery-item"><img src="/media/_PCV Gym.jpg"><p>The on-site gym is a great way to keep fit and healthy  9 of 10</p></div></div><div class="owl-item" style="width: 742px;"><div class="gallery-item"><img src="/media/_PCV Exterior Traffic Long Exposure 6x4.jpg"><p>Situated on Goldhawk Road, Prince Consort Village benefits from excellent local transport links  10 of 10</p></div></div></div></div>
+            
+
+                
+
+                
+
+
+                
+
+
+                
+
+                
+
+
+                
+
+                
+
+
+
+            
+     
+
+            
+
+        
+        
+        
+        </div>
+        <div class="owl-icons gallery-arrows">
+          <i class="float-right icon-arrow-right gallery-left"></i>
+          <i class="float-right icon-arrow-left gallery-right"></i>
+        </div>
+     </div>
+</div>
+        
+<div class="main-page-content">
+<h3 class="h3" name="">Facilities</h3>
+</div><div class="main-page-content" id="">
+ <p>Prince Consort Village is fully equipped and specially&nbsp;designed for RCM students, with&nbsp;acoustically treated&nbsp;bedrooms&nbsp;allowing you to practise at home. Services&nbsp;include:</p>
+<ul>
+<li>23 dedicated practice rooms available 24/7</li>
+<li>A&nbsp;film room</li>
+<li>Open mic space</li>
+<li>High speed Wi-Fi</li>
+<li>Laundry</li>
+<li>Reception</li>
+<li>Gym</li>
+<li>Bicycle storage</li>
+<li>Indoor and outdoor&nbsp;communal spaces</li>
+</ul>
+</div><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1241.8551610144068!2d-0.23761594686081158!3d51.50018312650322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1485800735392" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen=""></iframe><div class="main-page-content" id="">
+ <h3>Location</h3>
+<p>Prince Consort&nbsp;Village is located in leafy south-west London and within easy travelling distance of the RCM. It takes around 40 minutes door-to-door using the London Underground between&nbsp;Ravenscourt Park and South Kensington.</p>
+<p>The neighbourhood is ideal, offering the greenery of nearby&nbsp;<strong>Ravenscourt Park</strong>&nbsp;as well as the convenience of many&nbsp;local shops, cafes and amenities. Prince Consort Village is within easy reach of Westfield in <strong>Shepherd’s Bush</strong>, which houses leading fashion, home&nbsp;and lifestyle brands alongside cinemas and a fantastic&nbsp;selection of places to eat. Notting&nbsp;Hill – famous for&nbsp;<strong>Notting Hill Carnival</strong>&nbsp;as well as Portobello Road&nbsp;and its market – is only a short bus ride away. This part of London is arty and bohemian,&nbsp;with lots of restaurants, bars, shops and music venues.</p>
+</div><div class="row">
+	<div class="small-12 columns">
+		<div class="gallery  owl-carousel owl-theme" style="opacity: 1; display: block;">
+
+<div class="owl-wrapper-outer"><div class="owl-wrapper" style="width: 5936px; left: 0px; display: block; transition: all 0ms ease; transform: translate3d(0px, 0px, 0px);"><div class="owl-item" style="width: 742px;"><div class="gallery-item"><img src="/media/rcmacuk/6by4.jpg"><p>Leafy Ravenscourt Park  1 of 4</p></div></div><div class="owl-item" style="width: 742px;"><div class="gallery-item"><img src="/media/rcmacuk/6by4.jpg"><p>Notting Hill Carnival is one of the world's largest and most vibrant street festivals  2 of 4</p></div></div><div class="owl-item" style="width: 742px;"><div class="gallery-item"><img src="/media/rcmacuk/styleassests/6by4.jpg"><p>Portobello Road is famous for its architecture, its culture and its market  3 of 4</p></div></div><div class="owl-item" style="width: 742px;"><div class="gallery-item"><img src="/media/rcmacuk/styleassests/6by4.jpg"><p>Westfield is a major shopping and entertainment destination in Shepherd's Bush  4 of 4</p></div></div></div></div>
+            
+
+                
+
+                
+
+
+                
+
+
+                
+
+                
+
+
+                
+
+                
+
+
+
+            
+     
+
+            
+
+        
+        
+        
+        </div>
+        <div class="owl-icons gallery-arrows">
+          <i class="float-right icon-arrow-right gallery-left"></i>
+          <i class="float-right icon-arrow-left gallery-right"></i>
+        </div>
+     </div>
+</div>
+        
+<div class="main-page-content" id="">
+ <p>There are several <a href="https://tfl.gov.uk/maps?Input=Paddenswick%20Road&amp;InputGeolocation=51.500007,-0.237021" target="_blank">bus stops</a> conveniently located outside Prince Consort Village providing connections to Hammersmith, Shepherd’s Bush, Notting Hill and a range of other locations.</p>
+<p>The area is well served by trains too, with three stations nearby. <a href="https://tfl.gov.uk/tube/stop/940GZZLURVP/ravenscourt-park-underground-station" target="_blank">Ravenscourt Park</a> station on the London Underground's District line is a 10 minute walk away. <a href="https://tfl.gov.uk/tube/stop/940GZZLUGHK/goldhawk-road-underground-station?lineId=circle" target="_blank">Goldhawk Road</a> station on the London Underground's Circle and Hammersmith &amp; City lines is a 10 minute walk or a 5 minute bus ride away. <a href="https://tfl.gov.uk/tube/stop/940GZZLUSBC/shepherds-bush-central-underground-station" target="_blank">Shepherd’s Bush</a> station on the London Underground's Central line and the London Overground is a 20 minute walk or 10 minute bus ride away.</p>
+</div><div class="main-page-content">
+<h2 class="h2" name="">Privately rented accommodation</h2>
+</div><div class="main-page-content" id="">
+ <p>Lots of RCM students, particularly after their first year, choose to live in privately rented accommodation. London has a large rental market offering a range of properties in locations across the city.</p>
+<p>During the year our Student Services team produces a list of landlords offering private accommodation to RCM students.</p>
+</div><div class="main-page-content" id="">
+ <p>With effect from 20 February 2016 private landlords will be required to check their tenants' immigration status before granting a tenancy agreement. This to make sure the tenant has valid permission to be in the UK.</p>
+<p>To be eligible you must meet one of the following conditions:</p>
+<ul>
+<li>You are an EEA/Swiss national.</li>
+<li>You have the right to be in the UK under EEA law (for example because you are the family member of an EEA/Swiss national).</li>
+<li>You have valid immigration permission to be in the UK.</li>
+</ul>
+<p>The landlord may need to see your passport and/or visa to verify your immigration status.</p>
+</div><div class="main-page-content" id="">
+ <p>If you would like more information on how the RCM can provide support for students seeking privately rented accommodation, please feel free to talk to the Student Services team.</p>
+<p><strong>[SPACE FOR CONTACT CARD]</strong></p>
+</div>
+<div class="grey-20-back add-gutter">
+	<div class="row grey-20-back">
+		<div class="morefromrcm-topbar medium-12 columns">
+          <div class="owl-icons">
+            <i class="float-right icon-arrow-right next"></i>
+            <i class="float-right icon-arrow-left previous"></i>
+          </div>
+          <h2>More From the RCM</h2>
+        </div>
+    </div>
+    <div class="row grey-20-back">
+        <div id="owl-example" class="owl-carousel owl-theme" style="opacity: 1; display: block;">
+
+ 
+          
+    <!-- BRING IN NEWS -->
+			
+             
+<div class="owl-wrapper-outer"><div class="owl-wrapper" style="width: 4380px; left: 0px; display: block; transition: all 0ms ease; transform: translate3d(0px, 0px, 0px);"><div class="owl-item" style="width: 365px;"><div class="columns shuffle-item">
+	<p><a href="http://www.rcm.ac.uk/about/news/">News</a></p>
+	<a href="http://www.rcm.ac.uk/about/news/all/2017-01-202017rodwilliamsmemorialconcertannounced.aspx"><img src="http://www.rcm.ac.uk/media/Collon.jpg"></a>
+	<h4><a href="http://www.rcm.ac.uk/about/news/all/2017-01-202017rodwilliamsmemorialconcertannounced.aspx">2017 Rod Williams Memorial Concert Announced</a></h4>
+    <p></p><p>This year’s Rod Williams Memorial Concert features Nicholas Collon and the Royal College of Music Symphony Orchestra.</p><p></p>
+</div></div><div class="owl-item" style="width: 365px;"><div class="columns shuffle-item">
+	<p><a href="http://www.rcm.ac.uk/events/listings/">What's on</a></p>
+	<a href="/events/listings/details/?id=1086944"><img src="http://www.rcm.ac.uk/included/eventimages/berentkorfker.jpg"></a>
+	<p class="index-title"><a href="/events/listings/details/?id=1086944">Music and Ideas: Rethinking Rests</a></p>
+    <p>09 February 2017, 5:15pm<br>
+</p>
+</div></div><div class="owl-item" style="width: 365px;"><div class="columns shuffle-item">
+	<p><a href="http://www.rcm.ac.uk/events/listings/">What's on</a></p>
+	<a href="/events/listings/details/?id=1125620"><img src="http://www.rcm.ac.uk/included/eventimages/weber.jpg"></a>
+	<p class="index-title"><a href="/events/listings/details/?id=1125620">Weber In London: Chamber Concert</a></p>
+    <p>10 February 2017, 7:00pm<br>
+</p>
+</div></div><div class="owl-item" style="width: 365px;"><div class="columns shuffle-item">
+	<p><a href="http://www.rcm.ac.uk/events/listings/">What's on</a></p>
+	<a href="/events/listings/details/?id=1100409"><img src="http://www.rcm.ac.uk/included/eventimages/elliotgrestynew.jpg"></a>
+	<p class="index-title"><a href="/events/listings/details/?id=1100409">RCM New Perspectives: John Adams at 70</a></p>
+    <p>09 February 2017, 7:30pm<br>
+</p>
+</div></div><div class="owl-item" style="width: 365px;"><div class="columns shuffle-item">
+	<p><a href="http://www.rcm.ac.uk/about/news/">News</a></p>
+	<a href="http://www.rcm.ac.uk/about/news/all/2017-02-03rcmcommunitymusicresearchpartnershipawardedahrcgrant.aspx"><img src="http://www.rcm.ac.uk/media/Jennie Henley 435x290.jpg"></a>
+	<h4><a href="http://www.rcm.ac.uk/about/news/all/2017-02-03rcmcommunitymusicresearchpartnershipawardedahrcgrant.aspx">RCM Community Music Research Partnership Awarded AHRC Grant</a></h4>
+    <p></p><p>The ‘Excellence, Inclusion and Intervention in Music’ project, conducted in partnership with York St John University, has been awarded £45,000 by the Arts and Humanities Research Council.</p><p></p>
+</div></div><div class="owl-item" style="width: 365px;"><div class="columns shuffle-item">
+	<p><a href="http://www.rcm.ac.uk/about/news/">News</a></p>
+	<a href="http://www.rcm.ac.uk/about/news/all/buckginhampalacegala2017.aspx"><img src="http://www.rcm.ac.uk/media/rcmacuk/content/images/Buckingham-Palace-435x290.jpg"></a>
+	<h4><a href="http://www.rcm.ac.uk/about/news/all/buckginhampalacegala2017.aspx">Royal Gala at Buckingham Palace celebrates the Royal College of Music </a></h4>
+    <p></p><p>HRH The Prince of Wales, president of the RCM, has hosted a gala to celebrate students, alumni and the work the RCM is doing to expand its reach.</p><p></p>
+</div></div></div></div>
+                        
+             
+								
+             
+
+                        
+             
+								
+             
+
+                        
+             
+					  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+         
+    <!-- ASIMUT EVENTS WITH EXCEPTION HANDLING -->
+			
+             
+
+                        
+             
+								
+             
+
+                        
+             
+								
+             
+
+                        
+             
+					  
+  
+       
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         </div>
+    </div>
+</div>
+
+      </div>
+
+        </div>
+
+
+<? include('./includes/footer.html');?>
+
